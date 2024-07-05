22ba5676c8a691ce0234bb0438b214676c7aae12
Ankit Tejwan <77053184+ankit-tejwan@users.noreply.github.com>
2024-07-05 18:35:08 +0530

    Update api_check.py

--- api_check.py
+++ api_check.py
@@ -6,20 +6,12 @@ import base64
 url = 'http://localhost:5000/predict'
 
 # Read the image file and encode it to base64
-with open(r'image.jpg', 'rb') as image_file:
+with open(r'ng_image.jpg', 'rb') as image_file:
     image_base64 = base64.b64encode(image_file.read()).decode('utf-8')
-
-threshold_val = 55
-
-user_message ='Hi there i wanna send you something special'
 
 # Create the JSON payload with the base64 encoded image
 payload = {
-    'image': image_base64,
-    'threshold_val': threshold_val,
-    'user_message': user_message
-
-
+    'image': image_base64
 }
 
 # Send the POST request with the JSON payload
