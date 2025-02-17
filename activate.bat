36ed452df8a3503401f291d827578259f2513543
Ankit Tejwan <77053184+ankit-tejwan@users.noreply.github.com>
2025-02-17 11:27:24 +0530

    Delete activate.bat

--- activate.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM 
-CALL C:\Users\User\anaconda3\Scripts\activate.bat  C:\Users\User\anaconda3\envs\pytorch
-
-REM
-CD C:\Users\User\Desktop\bolt_project
-REM 
-python backend_server.py
